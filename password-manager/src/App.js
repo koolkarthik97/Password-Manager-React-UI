89bef0c8a48b317c0b2e1879b0d9eaf3396a732c
koolkarthik97 <koolkarthik97@gmail.com>
2020-05-01 00:50:31 -0700

    UI form and sign up design

--- password-manager/src/App.js
+++ password-manager/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Maincontainer from "./Components/Maincontainer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 function App() {
   return <Maincontainer />;
--- password-manager/src/Components/Welcome.jsx
+++ password-manager/src/Components/Welcome.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 import { Navbar, Form, FormControl, Button } from "react-bootstrap";
+import "../Images/home_lock.jpg";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+} from "mdbreact";
 class Welcome extends Component {
   state = {};
   render() {
@@ -21,7 +31,83 @@ class Welcome extends Component {
         </Form>
       </Navbar>
     );
-    return <React.Fragment>{navbar_tag}</React.Fragment>;
+    let form_tag = (
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md="6">
+            <MDBCard>
+              <MDBCardBody>
+                <form>
+                  <p className="h4 text-center py-4">Sign up</p>
+                  <div className="grey-text">
+                    <MDBInput
+                      label="Your name"
+                      icon="user"
+                      group
+                      type="text"
+                      validate
+                      error="wrong"
+                      success="right"
+                    />
+                    <MDBInput
+                      label="Your email"
+                      icon="envelope"
+                      group
+                      type="email"
+                      validate
+                      error="wrong"
+                      success="right"
+                    />
+                    <MDBInput
+                      label="Confirm your email"
+                      icon="exclamation-triangle"
+                      group
+                      type="text"
+                      validate
+                      error="wrong"
+                      success="right"
+                    />
+                    <MDBInput
+                      label="Your password"
+                      icon="lock"
+                      group
+                      type="password"
+                      validate
+                    />
+                  </div>
+                  <div className="text-center py-4 mt-3">
+                    <MDBBtn color="cyan" type="submit">
+                      Register
+                    </MDBBtn>
+                  </div>
+                </form>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    );
+
+    let container_tag = (
+      <div
+        style={{ display: "flex", flexDirection: "row", paddingLeft: "10%" }}
+      >
+        <img
+          src={require("../Images/home_lock.jpg")}
+          height="400dp"
+          style={{ marginTop: "8rem", marginLeft: "10rem" }}
+        />
+        <div style={{ width: "100%", marginRight: "-60%", marginTop: "2rem" }}>
+          {form_tag}
+        </div>
+      </div>
+    );
+    return (
+      <React.Fragment>
+        {navbar_tag}
+        {container_tag}
+      </React.Fragment>
+    );
   }
 }
 
